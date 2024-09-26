caa2d90985fec1a295dd3b3a8ec2faeab622db3d
Aldrin Abello <aldrinabello03@gmail.com>
2024-09-26 21:17:34 +0800

    new viewmodel

---
+++ Models/ViewModels/TestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PELMS.Models.ViewModels
+{
+    public class UserScoreViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Score { get; set; }
+    }
+
+}
